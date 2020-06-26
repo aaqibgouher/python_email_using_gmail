1ebfd96028c3e7bf97bfb6bd6edf4e29dc5ed143
aaqibgouher <aaqibgouher@gmail.com>
2020-06-26 13:42:20 +0530

    updated app.js file

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ def send_email(email,sub,body):
     server.quit()
 
 send_email("nfraz007@gmail.com","checking smtp server","I have created a function which will take three parameter receiver's email,subject, and body")
+
+This is my first project. In this project , what i have did is i have sended email using python. so here , i have written a function which will take three parameter namely receiver's email,subject and body.
+simply we have to pass all these parameters in the send_email() function . and then we have imported two things : first is out smtplib which is simplr mail transfer protocol, and secondly i have imported config file. In  config, sender's email and pass will be there.and then simply we have to use some of the inbuilt function of smtp. then we can do it easily.
